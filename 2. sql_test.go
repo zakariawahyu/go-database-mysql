fdd4cce9b8ff80a6026c39bf2713987e4f278ff5
Zakaria Wahyu <zakarianur6@gmail.com>
2022-01-04 21:47:50 +0700

    Last insertID, Prepare Statement, DB Transaction

--- 2. sql_test.go
+++ 2. sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -247,4 +248,130 @@ func TestExcelSqlParameter(t *testing.T) {
 
 	defer db.Close()
 	fmt.Println("Sukses insert data")
+}
+
+/**
+Auto Increment
+- Kadang kita membuat sebuah table dengan id auto increment
+- Dan kadang pula, kita ingin mengambil data id yang sudah kita insert ke dalam MySQL
+- Sebenarnya kita bisa melakukan query ulang ke database dengan menggunakan SELECT LAST_INSERT_ID()
+- Tapi untungnya di golang ada cara yang lebih mudah
+- Kita bisa menggunakan function (Result)LastInsertId() untuk mendapatkan ID terakhir yang dibuat secara auto increment
+- Result adalah object yang dikembalikan ketika kita menggunakan function Exec
+*/
+
+func TestAutoIncrement(t *testing.T) {
+	db := GetConnection()
+	ctx := context.Background()
+
+	email := "zakaria@gmail.com"
+	comment := "test comment"
+	sqlQuery := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+
+	result, err := db.ExecContext(ctx, sqlQuery, email, comment)
+	if err != nil {
+		panic(err)
+	}
+
+	lastID, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+	fmt.Println("Last insert ID :", lastID)
+}
+
+/**
+Query atau Exec dengan Parameter
+- Saat kita menggunakan function Query atau Exec yang menggunakan parameter, sebenarnya implementasi dibahwahnya menggunakan feature Prepare Statement
+- Jadi tahapan pertama nya disiapkan terlebih dahulu, setelah itu baru di isi dengan parameter
+- Kadang ada kasus kita ingin melakukan beberapa hal yang sama sekaligus, hanya berbeda parameter. Misal insert dengan data banyak sekaligus
+- Pembuatan Prepare Statement bisa dilakukan manual, tanpa harus menggunakan Query atau Exec dengan parameter
+
+Prepare Statement
+- Saat kita membuat Prepare Statement, secara otomatis akan mengenali koneksi database yang digunakan
+- Sehingga ketika kita mengeksekusi Prepare Statement berkali-kali, maka akan menggunakan koneksi yang sama dan lebih efisien karena pembuatan prepare statementnya hanya sekali diawal saja
+- Jika menggunakan Query dan Exec dengan parameter, kita tidak bisa menjamin bahwa koneksi yang digunakan akan sama, oleh karena itu, bisa jadi prepate statement akan selalu dibuat
+berkali-kali walaupun kita menggunakan SQL yang sama
+- Untuk membuat Prepare Statement, kita bisa menggunakan function (DB)Prepare(context, sql)
+- Prepare Statement direpresentasikan dalah struct database/sql.stmt
+- Sama seperti resource sql lainya, Stmt harus di close(), jika tidak digunakan lagi
+
+Jika insert banyak dengan parameter, disarankan menggunakan prepare statement
+*/
+
+func TestPrepareStatement(t *testing.T) {
+	db := GetConnection()
+	ctx := context.Background()
+
+	sqlQuery := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+	statement, err := db.PrepareContext(ctx, sqlQuery)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		email := "zakaria" + strconv.Itoa(i) + "@gmail.com"
+		comment := "Ini comment ke " + strconv.Itoa(i)
+
+		statement, err := statement.ExecContext(ctx, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		lastID, err := statement.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Comment ID :", lastID)
+	}
+	defer statement.Close()
+	defer db.Close()
+}
+
+/**
+Database Transaction di Go-Lang
+- Secara default semua perintah SQL yang kita kirim menggunakan GO-Lang akan otomatis commit atau istilahnya auto commit
+- Artinya setiap mengirim perintah SQL ke database akan di commit secara permanent di database
+- Namun kita bisa menggunakan fitur transaksi sehingga SQL yang kita kirim tidak secara otomatis di commit ke database
+- Untuk memulai transaksi, kita bisa menggunakan function (DB)Begin(), dimana akan menghasilkan struct TX yang merupakan representasi dari Transaction
+- Struct Tx ini yang kita gunakan sebagai pengganti DB untuk melakukan transaksi, dimana hampir semua  function di DB ada di TX SEPERTI Exec, Query atau Prepare
+- Setelah selesai proses transaksi, kita bisa gunakan function (Tx)Commit() untuk melakukan commit atau Rollback() untuk membatalkan commit atau mematalkan proses database
+*/
+
+func TestDbTransaction(t *testing.T) {
+	db := GetConnection()
+	ctx := context.Background()
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	sqlQuery := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+
+	for i := 0; i < 10; i++ {
+		email := "zakaria" + strconv.Itoa(i) + "@gmail.com"
+		comment := "Ini comment ke " + strconv.Itoa(i)
+
+		statement, err := tx.ExecContext(ctx, sqlQuery, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		lastID, err := statement.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Comment ID :", lastID)
+	}
+
+	//err = tx.Commit()
+	err = tx.Rollback() // Membatalkan eksekusi
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
